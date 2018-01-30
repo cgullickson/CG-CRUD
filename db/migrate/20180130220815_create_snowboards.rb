6f97edc294409e132e14aadbeed83eab59d9d93f
Chris Gullickson <cgullickson5@gmail.com>
2018-01-30 22:14:03 +0000

    created migrations

--- db/migrate/20180130220815_create_snowboards.rb
+++ db/migrate/20180130220815_create_snowboards.rb
@@ -1,4 +1,9 @@
 class CreateSnowboards < ActiveRecord::Migration
   def change
+    create_table :projects do |t|
+      t.string :name
+      t.string :brand
+      t.integer :user_id
+    end
   end
 end
--- db/migrate/20180130220826_create_gear.rb
+++ db/migrate/20180130220826_create_gear.rb
@@ -1,4 +1,9 @@
 class CreateGear < ActiveRecord::Migration
   def change
+    create_table :gear do |t|
+      t.string :name
+      t.string :type
+      t.integer :project_id
+    end
   end
 end
